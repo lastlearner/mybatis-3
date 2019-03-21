8961f60f1bb327f8716ac9fd9479d471b1421963
lyling <429825736@qq.com>
2019-03-21 09:58:18 +0800

    学习MyBatis源码第一天：    项目一览

--- src/test/java/org/apache/ibatis/autoconstructor/AutoConstructorMapper.java
+++ src/test/java/org/apache/ibatis/autoconstructor/AutoConstructorMapper.java
@@ -23,15 +23,32 @@ public interface AutoConstructorMapper {
   @Select("SELECT * FROM subject WHERE id = #{id}")
   PrimitiveSubject getSubject(final int id);
 
+  /*
+   *  PrimitiveSubject对应Subject表  PrimitiveSubject与AnnotatedSubject不同，在其构造方法上，weight和height方法参数的类型是int，而不是Integer。
+   *  如果subject表中的记录，这两个字段为NULL时，会创建PrimitiveSubject对象报错
+   */
   @Select("SELECT * FROM subject")
   List<PrimitiveSubject> getSubjects();
 
+  /*
+   *  AnnotatedSubjectd对应Subject表
+   */
   @Select("SELECT * FROM subject")
   List<AnnotatedSubject> getAnnotatedSubjects();
 
+  /**
+   *  BadSubject对应的也是subject表
+   *  和AnnotatedSubject不同，在其构造方法上，height方法参数的类型是height，而不是Integer
+   *  因为MyBatis无法识别Height类，所以会创建BadSubject对象报错
+   *  一般情况下，POJO对象里，不使用基本类型
+   */
   @Select("SELECT * FROM subject")
   List<BadSubject> getBadSubjects();
 
+  /*
+   *  ExtensiveSubject对应extensive_subject表
+   *  这是个复杂对象，基本涵盖了各种类型的数据
+   */
   @Select("SELECT * FROM extensive_subject")
   List<ExtensiveSubject> getExtensiveSubject();
 }
